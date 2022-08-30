8767168d6501e4ce1e11a9b66eaf2979e2a9e89c
Marat Chochaev <cool.mara007@yandex.ru>
2022-08-30 12:50:52 +0300

    small fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/marat-chochaev",
+  base: "/frontend-portfolio/",
   plugins: [vue()],
   resolve: {
     alias: {
